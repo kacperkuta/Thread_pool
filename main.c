a6790cd633aea2f7cdc1e3b21a141a0b5ecdba8b
Kacper Kuta <k.kuta2@student.uw.edu.pl>
2020-01-03 18:43:34 +0100

    Basically tested - thread_pool

--- main.c
+++ main.c
@@ -1,12 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "threadpool.h"
+
+void f(void* arg, size_t argsz) {
+    printf("testy\n");
+}
+
+void f1(void* arg, size_t argsz) {
+    printf("testy1\n");
+}
+
+void f2(void* arg, size_t argsz) {
+    printf("testy2\n");
+}
+
+void f3(void* arg, size_t argsz) {
+    printf("testy3\n");
+}
 
 int main() {
 
     thread_pool_t* pool = malloc(sizeof(thread_pool_t));
     thread_pool_init(pool, 5);
+
+    runnable_t r;
+    r.function = &f;
+    r.argsz = 0;
+    r.arg = NULL;
+    defer(pool, r);
+
+    runnable_t r1;
+    r1.function = &f1;
+    r1.argsz = 0;
+    r1.arg = NULL;
+    defer(pool, r1);
+
+    runnable_t r2;
+    r2.function = &f2;
+    r2.argsz = 0;
+    r2.arg = NULL;
+    defer(pool, r2);
+
+    runnable_t r3;
+    r3.function = &f3;
+    r3.argsz = 0;
+    r3.arg = NULL;
+    defer(pool, r3);
+    
+    
+    //sleep(3);
     thread_pool_destroy(pool);
+
+
 
     return 0;
 }
--- threadpool.h
+++ threadpool.h
@@ -34,10 +34,13 @@ typedef struct thread_pool {
     pthread_t* th;
     pthread_mutex_t m1;     //inicjalizacja w init
     pthread_cond_t c1;      //inicjalizacja w init
+    size_t onCondition;     //wątki na c1
+    pthread_cond_t c2;      //inicjalizacja w init
     element* queue;
     element* last;
     pthread_attr_t attr;
     int still_work;   //1 - ture, 0 - false
+    int poolDelete;   //1 - ture, 0 - false
 } thread_pool_t;
 
 runnable_t* pop(thread_pool_t* pool) {
@@ -56,6 +59,7 @@ runnable_t* pop(thread_pool_t* pool) {
 }
 
 void push(thread_pool_t* pool, void (*function)(void*, size_t), void* arg, size_t argsz) {
+
     runnable_t* toPush = malloc(sizeof(runnable_t));
     toPush->function = function;
     toPush->arg = arg;
@@ -84,13 +88,24 @@ void* work(void* pool_pointer) {
             syserr(err, "Error in lock");
         }
         runnable_t* toDo = NULL;
-        while (pool->still_work && (toDo = pop(pool)) == NULL) {
-            if ((err = pthread_cond_wait(&(pool->c1), &(pool->m1))) == 0) {
+        while (pool->still_work && toDo == NULL) {
+            pool->onCondition++;
+            if (pool->poolDelete && pool->onCondition == pool->size) {
+                if ((err = pthread_cond_signal(&(pool->c2))) != 0) {
+                    syserr(err, "Signal error.");
+                }
+            }
+            if ((err = pthread_cond_wait(&(pool->c1), &(pool->m1))) != 0) {
                 syserr(err, "Error in wait");
             }
+            pool->onCondition--;
+            toDo = pop(pool);
         }
-        if (toDo && pool->still_work)
+        if ((err = pthread_mutex_unlock(&pool->m1)) != 0)
+            syserr (err, "unlock failed");
+        if (toDo) {
             (toDo->function)(toDo->arg, toDo->argsz);
+        }
     }
     return 0;
 }
@@ -98,17 +113,21 @@ void* work(void* pool_pointer) {
 int thread_pool_init(thread_pool_t *pool, size_t pool_size) {
     pool->size = pool_size;
     pool->th = malloc(sizeof(pthread_t)*pool_size);
-    pool->still_work = true;
+    pool->still_work = 1;
+    pool->poolDelete = 0;
+    pool->onCondition = 0;
     pool->last = NULL;
     pool->queue = NULL;
     int err;
 
     if ((err = pthread_mutex_init(&pool->m1, 0) != 0)) {
         syserr(err, "mutex init failed");
-        return ERR;
-    } if ((err = pthread_cond_init(&pool->c1, 0)) != 0) {
-        syserr(err, "cond init failed");
-        return ERR;
+    }
+    if ((err = pthread_cond_init(&pool->c1, 0)) != 0) {
+        syserr(err, "cond1 init failed");
+    }
+    if ((err = pthread_cond_init(&pool->c2, 0)) != 0) {
+        syserr(err, "cond2 init failed");
     }
 
     if ((err = pthread_attr_init (&pool->attr)) != 0)
@@ -123,18 +142,37 @@ int thread_pool_init(thread_pool_t *pool, size_t pool_size) {
 
 void thread_pool_destroy(thread_pool_t *pool) {
     int err;
+    pool->poolDelete = true;
+
+    if ((err = pthread_mutex_lock(&(pool->m1))) != 0) {
+        syserr(err, "Error in lock");
+    }
+    while (pool->onCondition < pool->size) {
+
+        if ((err = pthread_cond_wait(&(pool->c2), &(pool->m1))) != 0) {
+            syserr(err, "Wait error.");
+        }
+    }
+
     pool->still_work = false;
     void* res;
     for (size_t i = 0; i < pool->size; i++) {
         if ((err = pthread_cond_signal(&pool->c1)) != 0) {
             syserr(err, "signal failed");
         }
+    }
+
+    if ((err = pthread_mutex_unlock(&(pool->m1))) != 0) {
+        syserr(err, "Error in unlock");
+    }
+
+    for (size_t i = 0; i < pool->size; i++) {
         if ((err = pthread_join((pool->th)[i], &res)) != 0)
             syserr(err, "join failed");
     }
 
     free(pool->th);
-    while(pool->queue) {
+    while (pool->queue) {
         element* n = (pool->queue)->next;
         free(pool->queue);
         pool->queue = n;
@@ -143,23 +181,28 @@ void thread_pool_destroy(thread_pool_t *pool) {
         syserr(err, "mutex destroy failed");
     }
     if ((err = pthread_cond_destroy(&pool->c1)) != 0) {
-        syserr(err, "cond destroy failed");
+        syserr(err, "cond1 destroy failed");
+    }
+    if ((err = pthread_cond_destroy(&pool->c2)) != 0) {
+        syserr(err, "cond2 destroy failed");
     }
     if ((err = pthread_attr_destroy(&pool->attr)) != 0)
         syserr(err, "attr destroy failed");
 }
 
 int defer(thread_pool_t *pool, runnable_t runnable) {
+    if(pool->poolDelete)
+        return ERR;
     int err;
     if ((err = pthread_mutex_lock(&(pool->m1))) != 0) {
         syserr(err, "Error in lock.");
-        return ERR;
     }
     push(pool, runnable.function, runnable.arg, runnable.argsz);
     if ((err = pthread_cond_signal(&(pool->c1))) != 0) {
         syserr(err, "Signal error.");
-        return ERR;
     }
+    if ((err = pthread_mutex_unlock(&pool->m1)) != 0)
+        syserr (err, "unlock failed");
     return SUCC;
 };
 
